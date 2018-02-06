2b4adb850cbb35a7f846762268de3dc60dfde9cc
Vishal <vishal2421@outlook.com>
2018-02-06 23:15:02 +0530

    Activity1 JSDemo

---
+++ Activity1/src/main/java/com/sapient/client/ClientA.java
@@ -0,0 +1,23 @@
+package com.sapient.client;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sapient.service.ISer;
+import com.sapient.service.ServiceImpl;
+
+public class ClientA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> lst = new ArrayList<String>();
+		lst.add("abc");
+		lst.add("hgr");
+		lst.add("dbc");
+		lst.add("alc");
+		lst.add("abc");
+		ISer ser = ServiceImpl.getInstance();
+		System.out.println(ser.findDuplicate(lst));
+	}
+
+}
